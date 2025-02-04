16bceb4e2d7c1d0d9f8692af6d1a75124ad50f01
Axemasta <33064621+Axemasta@users.noreply.github.com>
2025-02-04 17:51:46 +0000

    Add reproduction of net9 mct release crash

---
+++ src/ToolkitNet9Release/ToolkitNet9Release/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ToolkitNet9Release;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/ToolkitNet9Release/ToolkitNet9Release/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ToolkitNet9Release;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
